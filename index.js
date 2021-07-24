fcdae3a9879c078bbe45c75c2471ec056ab08dec
LilyCrown999 <adexadexdede@gmail.com>
2021-07-24 21:09:40 +0100

    done

--- index.js
+++ index.js
@@ -2,19 +2,17 @@
 const winston = require('winston');
 const express = require('express');
 const app = express();
-// const logger = require('./startup/logging');
 
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/logging');
 require('./startup/config')();
 require('./startup/validation')();
-const port = process.env.PORT || 1400;
-app.listen(port,
+require('./startup/prod')(app);
 
-    () => winston.info(`Listening on port ${port}...`)
-    
-    );
+const port = process.env.PORT || 1400;
+const server = app.listen(port,() => winston.info(`Listening on port ${port}...`));
 
     // app.use(express.static());
 
+module.exports = server;
--- startup/logging.js
+++ startup/logging.js
@@ -3,15 +3,15 @@ const winston = require('winston');
 require('express-async-errors');
 
 module.exports = function() {
-  winston.handleExceptions(
-    new winston.transports.Console({ colorize: true, prettyPrint: true }),
-    new winston.transports.File({ filename: 'path\to\ uncaughtExceptions.log' }));
+  winston.exceptions.handle()(
+    
+    new winston.transports.Console({colorize: true, prettyPrint: true , timeStamp : true}),
+    new winston.transports.File({ filename: 'path/to/ uncaughtExceptions.log'}));
   
   process.on('unhandledRejection', (ex) => {
     throw ex;
   });
-  
-  winston.add(winston.transports.File, { filename: 'path\to\logfile.log' });
+  winston.add(winston.transports.File, { filename: 'path/to/logfile.log' });
   // winston.add(winston.transports.MongoDB, { 
   //   db: 'mongodb://localhost/vidly',
   //   level: 'info'

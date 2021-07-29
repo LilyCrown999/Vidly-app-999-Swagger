28bdb8119a5ac75a07ece05f890c3cde4ef31829
LilyCrown999 <adexadexdede@gmail.com>
2021-07-29 22:12:16 +0100

    Trying swaggerUI

--- startup/routes.js
+++ startup/routes.js
@@ -8,19 +8,42 @@ const movies = require('../routes/movies');
 const rentals = require('../routes/rentals');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
-// const returns = require('../routes/returns');
+ 
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+
+const options = {
+    definition : {
+        openapi : '3.0.0',
+        info : {
+          title : 'Library API',
+          version : '1.0.0',
+          description : 'A simple Express Library API for vidly-app'
+        },
+        servers :[
+            {
+                url : 'https://vidly-app-999.herokuapp.com'
+            },
+        ],
+    },
+        apis : ['*../routes/*.js']
+};
+
+const specs = swaggerJsDoc(options); 
 
 
 module.exports = function (app){
     
 app.use(express.json());
-app.use('/vidly/genres', genres);
-app.use('/vidly/users', users);
-app.use('/vidly/customers', customers);
-app.use('/vidly/movies', movies);
-app.use('/vidly/rentals', rentals);
-app.use('/vidly/auth', auth);  
-// app.use('/api/returns', returns);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+// app.use('/vidly/genres', genres);
+// app.use('/vidly/users', users);
+// app.use('/vidly/customers', customers);
+// app.use('/vidly/movies', movies);
+// app.use('/vidly/rentals', rentals);
+// app.use('/vidly/auth', auth);  
+
 app.use(error);
 
 };

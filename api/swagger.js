f7abd6522b150992d817503e7911f80677fa7da2
LilyCrown999 <adexadexdede@gmail.com>
2021-09-15 00:39:16 +0100

    Added wworking post:/api/genres

--- api/swagger.js
+++ api/swagger.js
@@ -1,28 +1,5 @@
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     Book:
- *       type: object
- *       required:
- *         - title
- *         - author
- *       properties:
- *         id:
- *           type: string
- *           description: The auto-generated id of the book
- *         title:
- *           type: string
- *           description: The book title
- *         author:
- *           type: string
- *           description: The book author
- *       example:
- *         id: d5fE_asz
- *         title: The New Turing Omnibus
- *         author: Alexander K. Dewdney
- */
+
 
 /**
  * @swagger
@@ -32,7 +9,6 @@
  *       type: object
  *       required:
  *         - name
- *         - x-auth-token
  *       properties:
  *         id:
  *           type: string
@@ -40,13 +16,8 @@
  *         name:
  *           type: string
  *           description: The name of the Genre 
- *         x-auth-token:
- *            type: string
- *            description: The auth token only give to admin(s) login as a user using /api/auth to get then input at the header...
  *       example:
- *         id: 6140cb7ed820e013bc98f5d3
  *         name: Horror
- *         x-auth-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTQwY2I3ZWQ4MjBlMDEzYmM5OGY1ZDMiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2MzE2MzY0ODF9.Ru0unKQ2T8HyeJNopeKxPI9__8VdqrIAGRinBj8ZEZk
  */
 
  /**
@@ -74,28 +45,7 @@
  * 
  */
 
-/**
- * @swagger
- * /books:
- *   post:
- *     summary: Create a new book
- *     tags: [Books]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Book'
- *     responses:
- *       200:
- *         description: The book was successfully created
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Book'
- *       500:
- *         description: Some server error
- */
+
 
 /**
  * @swagger
@@ -103,6 +53,13 @@
  *   post:
  *     summary: Create a new genre
  *     tags: [Genres]
+ *     parameters:
+ *      - in: header
+ *        name: x-auth-token
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: Get this by taking the token in the header when create new user at post:/api/user then  login the user at post:/api/auth...
  *     requestBody:
  *       required: true
  *       content:
@@ -118,4 +75,6 @@
  *               $ref: '#/components/schemas/Genre'
  *       500:
  *         description: Some server error
+ *       401:
+ *         description: You are not an admin only admin(s) are allowed to create a genre 
  */
